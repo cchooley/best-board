65a49f2609b6fd4437e2101bb052cb25f1d3e84c
Conor Hooley <cchooley@gmail.com>
2018-07-16 10:22:06 -0600

    Logo styled

--- src/components/landing.js
+++ src/components/landing.js
@@ -8,7 +8,7 @@ const Landing = (props) => {
         <div class="mainLand">
             <div class="innerLand">
                 <h2 class="big gold">Get in motion.</h2>
-                <h3>Welcome to <span class="gold">BestBoard</span>, the number one new app for managing productivity for non-profit B-O-Ds.</h3>
+                <h3>Welcome to <span class="gold">BestBoard</span>, the number one new app for managing productivity in non-profit governance.</h3>
                 <h3>Create your own account or login below.</h3>
                 <div class="landButton">
                     <Register />
